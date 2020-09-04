0150452a97b2d84c12ca417f86541781c26dcccc
Yuri Possokhov <possokhoff@gmail.com>
2020-09-04 21:49:10 +0300

    Ops methods are added

---
+++ R/drift_algebra.R
@@ -0,0 +1,12 @@
+#' @export
+Ops.drift <- function(e1, e2 = NULL){
+  checkmate::assert_class(e1, "drift")
+  checkmate::assert_multi_class(e2, c("integer", "numeric", "matrix", "drift"))
+  switch(
+    class(e2),
+    integer = checkmate::assert_number(e2, finite = TRUE),
+    numeric = checkmate::assert_number(e2, finite = TRUE),
+    drift = checkmate::assert_true(all(rownames(e1) == rownames(e2)))
+  )
+  NextMethod("Ops")
+}
