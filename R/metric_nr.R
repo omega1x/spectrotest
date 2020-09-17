14ec491acf1d007c265661be9267568baeb7ffff
omega1x <omega1x@gmail.com>
2020-09-17 18:58:41 +0300

    nr is added

--- R/metric_nr.R
+++ R/metric_nr.R
@@ -1,7 +1,51 @@
+#' @title
+#'   Convert DRIFT-spectrum to normalized radiance units
+#'
+#' @family metric
+#'
+#' @description
+#'  Convert raw DRIFT-spectrum recorded in transmittance mode (i.e. reflectance)
+#'  to normalized radiance units expressed in units of power as it calculated
+#'  by \code{\link{planck}}.
+#'
+#' @param x
+#'   an object of \code{\link{S3-class}} \emph{drift}.
+#'
+#' @return
+#'   an object of \code{\link{S3-class}} \emph{drift} exposed
+#'   in \href{https://en.wikipedia.org/wiki/International_System_of_Units}{SI}-units
+#'   of power: \eqn{W \cdot m^{-2} \cdot sr^{-1} \cdot \left ( {cm^{-1}} \right )^{-1}}{Wm^(-2)sr^(-1)(1/cm)^(-1)}.
+#'
+#' @details
+#'   Only raw DRIFT-spectra recorded into \emph{3709} channels within
+#'   \emph{349.1157 - 7501.1654} \eqn{cm^{-1}} infrared range could be processed.
+#'
+#' @references
+#'  \emph{Infrared Analysis of Particulates by FT-IR Emission/Transmission
+#'  Spectroscopy}. Peter R. Solomon et al., \emph{Applied Spectroscopy},
+#'  Volume: \emph{40} issue: \emph{6}, page(s): \emph{746-759}. DOI: \href{https://doi.org/10.1366/0003702864508421}{10.1366/0003702864508421}.
+#'
+#' @export
+#'
+#' @examples
+#' gx <- median(nr(coal_drift("GX")))
+#' oc <- median(nr(coal_drift("OC")))
+#'
+#' vs <- gx - oc  # volatile substances
+#' vs[["ftirValidFileName"]] <- "Volatile"
+#' plot(slice(cbind(vs, oc, gx),,,3700))
+#' abline(h = 0, col = "red")
+
 nr <- function(x) {
   checkmate::assert_class(x, "drift")
-  checkmate::assert_complex(x[['ftirPowerMax']], null.ok = FALSE)
-
+  # Note! Only spectra within 349.1157 - 7501.1654 1/cm range recorded
+  # in 3709 channels are allowed!
+  wave_numbers <- as.numeric(rownames(x))
+  checkmate::assert_true(length(wave_numbers) == 3709L)
+  # Note! Only raw spectra, i.e. recorded in %Transmittance mode, could be processed
+  checkmate::assert_string(x[["ftirIntensityMode"]], pattern = "%Transmittance")
+  max_power = x[['ftirPowerMax']]
+  checkmate::assert_complex(max_power, null.ok = FALSE)
   power <- try(
     utils::read.table(
       text = gsub("i", "i\n", x[["ftirPower"]], fixed = TRUE),
@@ -9,36 +53,41 @@ nr <- function(x) {
     silent = TRUE
   )
   checkmate::assert_complex(power, null.ok = FALSE)
-
   MAX_REFL_RANGE = c(1700, 2300)  # [1/cm]
-  wave_numbers <- as.numeric(rownames(x))
-  checkmate::assert_true(is.in(MAX_REFL_RANGE, range(wave_numbers)))
+  checkmate::assert_true(is.in(MAX_REFL_RANGE, range(wave_numbers)))  # always true?
 
-  # Find out peak of planck curve:
-  peak <- with(list(f <- function(x, x0)
-    planck(x, x0 = x0),
-    max_power <- x[["ftirPowerMax"]]), {
+  # Find out peak of Planck curve:
+  peak <- with(
+    list(f = function(x, x0) planck(x, x0 = x0)), {
+      # Fit Planck curve to real data:
       power <- c(power, max_power)
-      # Fit planck-curve to real data:
-      m <- nls(
+      m <- stats::nls(
         y ~ beta * f(x, x0),
         data = data.frame(x = wave_numbers[Im(power)], y = Re(power)),
         start = c(beta = 14, x0 = wave_numbers[Im(max_power)]),
-        lower = c(beta = 10, x0 = min(MAX_REFL_RANGE)),
-        upper = c(beta = 20, x0 = max(MAX_REFL_RANGE)),
+        lower = c(beta = 10, x0 = MAX_REFL_RANGE[1]),
+        upper = c(beta = 20, x0 = MAX_REFL_RANGE[2]),
         algorithm = "port"
       )
       coef(m)
     })
 
-  CO2 <- list(low = c(2268, 2277), high = c(2399, 2412))  # [1/cm]
+  co2_channels <- with(
+    list(
+      co2 = c(2268:2277, 2399:2412)  # [1/cm]
+    ), {
+    spectrotest::channel(x, co2[findInterval(co2, range(wave_numbers)) == 1])
+  })
+
   for (i in seq_len(ncol(x))) {
-    max_refl <- max(stats::smooth.spline(x[setdiff(rownames(x), CO2), i],
-                                  keep.data = FALSE)$y)
     cell <- as.double(x[, i])
-    x[, i] <- planck(wave_numbers,
-                     x0 = peak[["x0"]]) * (-cell / max_refl + 1)
+    max_refl <- max(
+      stats::smooth.spline(cell[-co2_channels], keep.data = FALSE)$y
+    )
+    x[, i] <-  spectrotest::planck(wave_numbers, x0 = peak[["x0"]]) *
+      (-cell / max_refl + 1)
   }
+  x[["ftirIntensityMode"]] <- "NormRadiance"
   x
 }
 
